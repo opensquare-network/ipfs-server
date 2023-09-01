456f77bf2a23faf79bb5a26163e73ee000fb8652
chaojun <chaojun@opensquare.network>
2023-09-01 15:10:35 +0800

    Clean up code

--- src/utils/ipfs.js
+++ src/utils/ipfs.js
@@ -1,5 +1,3 @@
-const Hash = require("ipfs-only-hash");
-const { CID } = require("multiformats/cid");
 const { getClients, ClientOptions, ClientMode } = require("@osn/ipfs");
 const {
   INFURA_PROJECT_ID,
@@ -35,26 +33,7 @@ async function ipfsAddBuffer(data) {
   return added;
 }
 
-async function cidOf(obj) {
-  const data = JSON.stringify(obj);
-  const cidV0 = await Hash.of(Buffer.from(data));
-  const cid = CID.parse(cidV0);
-  return cid.toV1().toString();
-}
-
-async function pinJsonToIpfsWithTimeout(data, timeout) {
-  const errorMsg = "Pin json to ipfs timeout";
-  return await Promise.race([
-    new Promise((_, reject) =>
-      setTimeout(() => reject(new Error(errorMsg)), timeout)
-    ),
-    ipfsAdd(data),
-  ]);
-}
-
 module.exports = {
-  cidOf,
   ipfsAdd,
   ipfsAddBuffer,
-  pinJsonToIpfsWithTimeout,
 };
